cf59be3f802e3abafec26f0b45639ffa63563fe5
adam <afenton@getmoxied.net>
2016-04-13 08:22:34 -0500

    Sample home template module

---
+++ src/Modules/Home/Bootstrap.php
@@ -0,0 +1,20 @@
+<?php namespace Leean\Modules\Home;
+
+use Leean\PageTemplates;
+
+/**
+ * Class Bootstrap
+ *
+ * @package Skaled
+ */
+class Bootstrap {
+
+	const TEMPLATE = 'home';
+
+	/**
+	 * Init
+	 */
+	public static function init() {
+		PageTemplates::register( self::TEMPLATE, 'Home' );
+	}
+}
