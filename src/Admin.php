4e7498e82a4e2398d4b2778c789c93b5ac491a01
mitogh <mitogh@gmail.com>
2016-05-02 12:46:20 -0500

    Rewrite filter used to hide menus

--- src/Admin.php
+++ src/Admin.php
@@ -13,17 +13,30 @@ class Admin
 			return;
 		}
 
-		add_action( 'admin_menu', function () {
-			$user = \wp_get_current_user();
+		add_action( 'admin_menu', [ __CLASS__, 'hide_menu_items' ] );
+	}
 
-			if ( in_array( 'administrator', $user->roles, true ) ) {
-				return;
-			}
+	/**
+	 * Function that hides menu items from the users that does not have
+	 * the administrator role.
+	 */
+	public static function hide_menu_items() {
+		if ( self::is_administrator( \wp_get_current_user() ) ) {
+			return;
+		}
+		remove_menu_page( 'tools.php' );
+		remove_menu_page( 'themes.php' );
+		remove_menu_page( 'edit-comments.php' );
+		remove_menu_page( 'edit.php' );
+	}
 
-			remove_menu_page( 'tools.php' );
-			remove_menu_page( 'themes.php' );
-			remove_menu_page( 'edit-comments.php' );
-			remove_menu_page( 'edit.php' );
-		} );
+	/**
+	 * Function that test if the user has the administrator role
+	 *
+	 * @param \WP_User $user The user to test.
+	 * @return bool true if the user has the administrator role false otherwise.
+	 */
+	public static function is_administrator( \WP_User $user ) {
+		return in_array( 'administrator', $user->roles, true );
 	}
 }
