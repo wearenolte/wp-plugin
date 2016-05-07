58b144f801a1ef24fbe9763d6ca4aa23d0df3130
adam <afenton@getmoxied.net>
2016-05-07 18:40:01 -0500

    Remove the blog routes (they're now in wp-endpoints-routes)

--- src/Api.php
+++ src/Api.php
@@ -16,37 +16,7 @@ class Api
 		Endpoints\Post::init();
 		Endpoints\StaticApi::init();
 
-		add_filter( 'ln_endpoints_data_routes', [ __CLASS__, 'add_extra_routes' ] );
-
 		add_filter( 'allowed_http_origin', [ __CLASS__, 'gform_allowed_http_origin' ] );
-	}
-
-	/**
-	 * Add extra routes the the endpoint
-	 *
-	 * @param array $routes The current set of routes.
-	 * @return mixed
-	 */
-	public static function add_extra_routes( $routes ) {
-		$extra_routes = [
-			[
-				'state' => 'blogIndex',
-				'url' => '/blog/',
-				'template' => 'blog',
-				'endpoint' => 'collection',
-				'params' => [
-					/* Add additional params here */
-				],
-			],
-			[
-				'state' => 'blogPost',
-				'url' => '/blog/:slug',
-				'template' => 'blog-single',
-				'endpoint' => 'post',
-			],
-		];
-
-		return array_merge( $routes, $extra_routes );
 	}
 
 	/**
