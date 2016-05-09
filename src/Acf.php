6dbd84b66160ad1add3c0567758d92f6ed8dc564
adam <afenton@getmoxied.net>
2016-05-09 16:29:43 -0500

    #28 Save ACF's as json

---
+++ src/Acf.php
@@ -0,0 +1,43 @@
+<?php namespace Leanp;
+
+/**
+ * Set-up ACF.
+ */
+class Acf
+{
+	/**
+	 * The folder in which JSON files are saved.
+	 *
+	 * @var string
+	 */
+	public static $json_folder = LEANP_PLUGIN_DIR . 'acf';
+
+	/**
+	 * Init.
+	 */
+	public static function init() {
+		add_filter( 'acf/settings/save_json', [ __CLASS__, 'save_json' ] );
+		add_filter( 'acf/settings/load_json', [ __CLASS__, 'load_json' ] );
+	}
+
+	/**
+	 * Get the path for saving ACF JSON.
+	 *
+	 * @return string
+	 */
+	public static function save_json() {
+		return self::$json_folder;
+	}
+
+	/**
+	 * Add our path to the locations to load ACF JSON from.
+	 *
+	 * @param array $paths The paths.
+	 * @return array
+	 */
+	public static function load_json( $paths ) {
+		unset( $paths[0] );
+		$paths[] = self::$json_folder;
+		return $paths;
+	}
+}
